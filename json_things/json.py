39afe641767314859d7458af46afeee9624f19bd
jfran <100429065@alumnos.uc3m.es>
2021-10-21 11:40:00 +0200

    arreglado la ruta de json

--- json_things/json.py
+++ json_things/json.py
@@ -1,9 +1,9 @@
-
+import json
 
 
 class Json:
     @staticmethod
     def datos_iniciar_sesion():
-        json_file = open("C:/Users/jfran/PycharmProjects/app-cripto/json_things/cuentas.json")
+        json_file = open("json_things/cuentas.json")
         data = json.load(json_file)
         return data
